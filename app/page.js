204de36f85bd5534f56a4a73c03d74372b16159f
Bircan Tanoba <bircantanoba@gmail.com>
2023-10-11 16:45:25 +0300

    .

--- app/page.js
+++ app/page.js
@@ -1,8 +1,4 @@
 "use client";
-import Input from "@/components/form/input";
-import Title from "@/components/ui/Title";
-import { loginSchema } from "@/schema/login";
-import { useFormik } from "formik";
 export default function Home() {
   return (
     <div className="w-full h-screen bg-secondary place-items-center place-content-center flex">
