057f7919274253f8167efead3a33f1570fbe646b
Bircan Tanoba <bircantanoba@gmail.com>
2023-10-11 16:06:24 +0300

    tsconfig updated

--- app/api/txt2img/route.js
+++ app/api/txt2img/route.js
@@ -1,8 +1,8 @@
-import { StableDiffusionAPI } from "@/services/api";
+import { StableDiffusionAPI } from "../../../services/api";
 
-const handler = async (req, res) => {
+const handler = async (req) => {
   const sdApi = new StableDiffusionAPI();
-  const response = await sdApi.apiV3Text2imgPost({
+  const { response } = await sdApi.apiV3Text2imgPost({
     key: "Ddoxn9JT0nOj6FQshX3LJosr8WrSbc01RepSubAp3Eu0JwP0aw72pX0jdqIP",
     prompt:
       "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
